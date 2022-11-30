2ad748f73f7106927e4cd21eed860c860fa01c4f
Francileudo Oliveira <sfrancileudo1@gmail.com>
2022-11-30 10:14:58 -0300

    style: styling the Banner component with styled-components

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,52 +1,51 @@
+import config from "../../../config.json"
+import styled from 'styled-components';
+
+const StyledBanner = styled.section`
+  background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(${config.imageBanner});
+  height: 20rem;
+  background-repeat: no-repeat;
+  background-position: bottom;
+  background-size: cover;
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  padding: 2rem;
+
+  h2 {
+    margin-bottom: 1.5rem;
+    font-size: 2rem;
+  }
+
+  p {
+    margin-bottom: 1.5rem;
+    font-size: 1rem;
+  }
+
+  button {
+    padding: 0.5rem 1rem;
+    background: var(--cor-1);
+    border: 2px solid var(--cor-1);
+    color: #fff;
+    transition: all 300ms ease-out;
+    width: 10rem;
+
+    &:hover {
+      background: var(--cor-2);
+      border-color: var(--cor-2);
+    }
+  }
+`;
+
 export function Banner() {
   return (
     <>
-      <section className="c-banner">
+      <StyledBanner>
         <h2>Promoção de Aniversário</h2>
         <p>Todos os produtos com 20% de desconto</p>
-        <button className="c-banner__button">Produtos</button>
-      </section>
-
-      <style jsx>{`
-        .c-banner {
-          background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url("https://images.unsplash.com/photo-1509366812838-b768dc7a8b3d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80");
-          height: 20rem;
-          background-repeat: no-repeat;
-          background-position: bottom;
-          background-size: cover;
-          color: #fff;
-          display: flex;
-          flex-direction: column;
-          justify-content: flex-end;
-          padding: 2rem;
-        }
-
-        .c-banner h2, .c-banner p {
-          margin-bottom: 2rem;
-        }
-
-        .c-banner h2 {
-          font-size: 2rem;
-        }
-
-        .c-banner p {
-          font-size: 1rem;
-        }
-
-        .c-banner__button {
-          padding: 0.5rem 1rem;
-          background: var(--cor-1);
-          border: 2px solid var(--cor-1);
-          color: #fff;
-          transition: all 300ms ease-out;
-          width: 10rem;
-        }
-
-        .c-banner__button:hover {
-          background: var(--cor-2);
-          border-color: var(--cor-2);
-        }
-      `}</style>
+        <button>Produtos</button>
+      </StyledBanner>
     </>
   )
 }
