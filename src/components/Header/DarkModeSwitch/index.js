e5663e758dea9c1874f9a07766817a0e64ab71ba
Francileudo Oliveira <sfrancileudo1@gmail.com>
2022-12-13 08:17:33 -0300

    feat: delete console.log

--- src/components/Header/DarkModeSwitch/index.js
+++ src/components/Header/DarkModeSwitch/index.js
@@ -42,7 +42,6 @@ export default function DarkModeSwitch() {
     return (
         <StyledSwitch>
             <input id="darkmode" type="checkbox" onChange={() => {
-                console.log("mudou o state");
                 contexto.toggleMode();
             }} />
             <label
